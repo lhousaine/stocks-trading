52b8f5f34425e837cf023f81ffa0f569e3ceadbb
lhousaine <l.ouarhou.ensa@gmail.com>
2022-09-12 22:06:36 +0100

    fix env variables vulnerability issues

--- src/apis/finnHub.js
+++ src/apis/finnHub.js
@@ -1,10 +1,8 @@
 import axios from 'axios';
 
-const TOKEN = 'ccetglqad3ifd4q5u9k0';
-
 export default axios.create({
   baseURL: 'https://finnhub.io/api/v1',
   params: {
-    token: TOKEN,
+    token: process.env.REACT_APP_TOKEN,
   },
 });
