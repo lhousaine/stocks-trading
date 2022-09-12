d1f093fbc64ce4d725a2b693a86f09bc479232f0
lhousaine <l.ouarhou.ensa@gmail.com>
2022-09-12 21:55:19 +0100

    add router for the application

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { StockOverviewPage } from './pages/StockOverviewPage';
+import { StockDetailsPage } from './pages/StockDetailsPage';
+import { WatchListContextProvider } from './context/watchListContext';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className='container'>
+      <WatchListContextProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path='/' element={<StockOverviewPage />} />
+            <Route path='/detail/:symbol' element={<StockDetailsPage />} />
+          </Routes>
+        </BrowserRouter>
+      </WatchListContextProvider>
+    </main>
   );
 }
-
-export default App;
