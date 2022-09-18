4af0decd9c1e8d1ed31447cac41970ebc1c31388
lhousaine <l.ouarhou.ensa@gmail.com>
2022-09-18 11:25:00 +0100

    feat(not Found):create not found page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { StockOverviewPage } from './pages/StockOverviewPage';
 import { StockDetailsPage } from './pages/StockDetailsPage';
 import { WatchListContextProvider } from './context/watchListContext';
+import NotFound from './components/NotFound';
 
 export default function App() {
   return (
@@ -13,6 +14,7 @@ export default function App() {
           <Routes>
             <Route path='/' element={<StockOverviewPage />} />
             <Route path='/detail/:symbol' element={<StockDetailsPage />} />
+            <Route path='*' element={<NotFound />}></Route>
           </Routes>
         </BrowserRouter>
       </WatchListContextProvider>
